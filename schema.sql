f035bba3fc0cc9e104a57baa65bab00f80413a38
root <junhyeokhan.it@gmail.com>
2017-06-22 08:43:46 +0200

    schema.sql update

--- schema.sql
+++ schema.sql
@@ -5,7 +5,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `app` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `app`;
 
 DROP TABLE IF EXISTS `category`;
